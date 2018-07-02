4036770539a98985a8dd0348ecdd3a40a87df55c
Raj Singh <rajsingh140186@googlemail.com>
2018-07-02 11:29:22 +0100

    Done #1

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import picture from './abm.jpg';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to Angular</h1>
+        <header className="App-header">    
+
+          <h1 className="App-title">Welcome to Kodflix</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>Hello World</code> and save to reload.
+        <p className="App-intro">        
+          To get started, edit <code>Hello World</code> and save to reload.       
+        <img src={picture} className="" alt="Logo" />;
         </p>
       </div>
     );
