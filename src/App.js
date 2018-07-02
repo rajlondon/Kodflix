c5375ecadecdd23176859927013b1ce6d8ed7188
Raj Singh <rajsingh140186@googlemail.com>
2018-07-02 15:26:17 +0100

    done #2

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import picture from './abm.jpg';
+import bb.jpg from "./Movies/bb.jpg"
+import BM.jpg from "./Movies/BM.jpg"
+import DE.jpg from "./Movies/DE.jpg"
+import game.jpg from "./Movies/game.jpg"
+import walking.jpg from "./Movies/walking.jpg"
+import wire.jpg from "./Movies/wire.jpg"
+
 
 class App extends Component {
   render() {
@@ -11,13 +17,27 @@ class App extends Component {
 
           <h1 className="App-title">Welcome to Kodflix</h1>
         </header>
+
+        <div className = "container">
+          <Image className="item">Black Mirror</Image>
+          <Image className="item">Breaking Bad</Image>
+          <Image className="item">Death Note</Image>
+        </div>
+
+        <div className = "container">
+          <Image className="item">Game of Thrones</Image>
+          <Image className="item">The Walking Dead</Image>
+          <Image className="item">The Wire</Image>
+        </div>
+
         <p className="App-intro">        
           To get started, edit <code>Hello World</code> and save to reload.       
-        <img src={picture} className="abm" alt="Logo" />;
+        
         </p>
       </div>
     );
   }
 }
+
 
 export default App;
