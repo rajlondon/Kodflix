914ab83e5e1ef198c1c54e6d5c9d16a074783a27
Raj Singh <rajsingh140186@googlemail.com>
2018-07-04 14:18:52 +0100

    Add the TV show title overlay on hover-over #5

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import bb from "./Movies/bb.jpg"
 import BM from "./Movies/BM.jpg"
@@ -13,27 +12,46 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">    
+        <header className="App-header">
 
-          <h1 className="App-title">Welcome to Kodflix</h1>
+          <h1 className="App-title">Welcome to Kodiflix</h1>
         </header>
 
-        <div className = "container">
-       <div className="item"><img src={bb} className="Movies" alt="Breaking Bad" /></div>
-       <div className="item"><img src={BM} className="Movies" alt="Black Mirror" /></div>
-       <div className="item"><img src={DE} className="Movies" alt="Death Note" /></div>
+        <div className="container">
+
+          <div className="item">
+            <img src={bb} className="Movies" alt="Breaking Bad" />
+            <div className="movieTitle"> <h3>Breaking Bad</h3></div>
+          </div>
+
+          <div className="item">
+            <img src={BM} className="Movies" alt="Black Mirror" />
+            <div className="movieTitle"> <h3>Black Mirror</h3></div>
+          </div>
+
+          <div className="item">
+            <img src={DE} className="Movies" alt="Death Note" />
+            <div className="movieTitle"><h3>Death Note</h3></div>
+          </div>
         </div>
 
-        <div className = "container">
-        <div className="item"><img src={game} className="Movies" alt="Game of Thrones" /></div>
-        <div className="item"><img src={walking} className="Movies" alt="The Walking Dead" /></div>
-        <div className="item"><img src={wire} className="Movies" alt="The Wire" /></div>
-        </div>
+        <div className="container">
 
-        <p className="App-intro">        
-          To get started, edit <code>Hello World</code> and save to reload.       
-        
-        </p>
+          <div className="item">
+            <img src={game} className="Movies" alt="Game of Thrones" />
+            <div className="movieTitle"> <h3>Game of Thrones</h3></div>
+          </div>
+
+          <div className="item">
+            <img src={walking} className="Movies" alt="The Walking Dead" />
+            <div className="movieTitle"> <h3>The Walking Dead</h3></div>
+          </div>
+
+          <div className="item">
+            <img src={wire} className="Movies" alt="The Wire" />
+            <div className="movieTitle"> <h3>The Wire</h3></div>
+          </div>
+        </div>
       </div>
     );
   }
